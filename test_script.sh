323cfbec7c3647c678a3a17887b54d1b3552e165
lyclyc52 <yliugu@connect.ust.hk>
2023-07-11 13:50:47 +0800

    sync

--- test_script.sh
+++ test_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=4
 ns-viewer --load-config /disk1/yliugu/lerf/outputs/teatime/samnerf-v2/contrastive_global_sample/config.yml
