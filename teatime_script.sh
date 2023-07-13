9a830c1884f1e1d939ee0ea1585f0861d3f27647
lyclyc52 <yliugu@connect.ust.hk>
2023-07-13 15:58:55 +0800

    sync

--- teatime_script.sh
+++ teatime_script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=3
 ns-train samnerf-v2 \
---data /disk1/yliugu/lerf/data/Datasets/teatime \
+--data /ssddata/yliugu/lerf/data/Datasets/teatime \
 --save_only_latest_checkpoint False \
 --pipeline.datamanager.disable-contrastive True \
 --project-name origin_global_sample
--- test_script.sh
+++ test_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=4
-ns-viewer --load-config /disk1/yliugu/lerf/outputs/teatime/samnerf-v2/contrastive_global_sample/config.yml
+export CUDA_VISIBLE_DEVICES=3
+ns-viewer --load-config ./outputs/teatime/samnerf-v2/contrastive_global_sample/config.yml
